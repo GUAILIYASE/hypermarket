07d3dd7dc4326126df334769fa9043bacd9a7e88
unknown <1483879208@qq.com>
2020-11-08 16:55:19 +0800

    初始化

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,37 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/layout/Home'
+import layout from '../views/layout/layout.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '',
+    name: 'layout',
+    component: layout,
+    children:[
+      {
+        path: '/',
+        name: 'Home',
+        component: Home
+      }
+    ]
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/login/login.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('../views/register/register.vue')
+  },
+  {
+    path: '*',
+    component: () => import( '../views/404/404.vue')
+  },
 ]
 
 const router = new VueRouter({
@@ -27,3 +41,8 @@ const router = new VueRouter({
 })
 
 export default router
+
+
+router.beforeEach((to, from, next) => {
+  console.log(to)
+})
